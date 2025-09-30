9139f22ec19ddaa210e960dd159837d0795c8b8b
Howard <62272939+juyoungIt@users.noreply.github.com>
2025-10-01 00:30:29 +0900

    Initial commit

---
+++ src/main/java/com/ktb/howard/ktb_community_server/KtbCommunityServerApplication.java
@@ -0,0 +1,13 @@
+package com.ktb.howard.ktb_community_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtbCommunityServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtbCommunityServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ktb/howard/ktb_community_server/KtbCommunityServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktb.howard.ktb_community_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtbCommunityServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
