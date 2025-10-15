fffe79cdea7df86a95b3e65b319185021c2bfd14
Howard <62272939+juyoungIt@users.noreply.github.com>
2025-10-16 05:36:42 +0900

    feat[#50]: 댓글 목록 조회 API 구현 (Offset 기반 Paging)

--- src/main/java/com/ktb/howard/ktb_community_server/comment/service/CommentService.java
+++ src/main/java/com/ktb/howard/ktb_community_server/comment/service/CommentService.java
@@ -1,13 +1,20 @@
 package com.ktb.howard.ktb_community_server.comment.service;
 
 import com.ktb.howard.ktb_community_server.comment.domain.Comment;
+import com.ktb.howard.ktb_community_server.comment.dto.CommentResponseDto;
 import com.ktb.howard.ktb_community_server.comment.dto.CreateCommentResponseDto;
+import com.ktb.howard.ktb_community_server.comment.repository.CommentQueryRepository;
 import com.ktb.howard.ktb_community_server.comment.repository.CommentRepository;
+import com.ktb.howard.ktb_community_server.image.domain.ImageType;
+import com.ktb.howard.ktb_community_server.image.dto.CreateImageViewUrlRequestDto;
+import com.ktb.howard.ktb_community_server.image.service.ImageService;
 import com.ktb.howard.ktb_community_server.member.domain.Member;
 import com.ktb.howard.ktb_community_server.member.repository.MemberRepository;
 import com.ktb.howard.ktb_community_server.post.domain.Post;
 import com.ktb.howard.ktb_community_server.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,8 +27,10 @@ import java.util.NoSuchElementException;
 public class CommentService {
 
     private final CommentRepository commentRepository;
+    private final CommentQueryRepository commentQueryRepository;
     private final PostRepository postRepository;
     private final MemberRepository memberRepository;
+    private final ImageService imageService;
 
     @Transactional
     public CreateCommentResponseDto createComment(
@@ -50,6 +59,17 @@ public class CommentService {
                 parentComment != null ? comment.getParentComment().getId() : null,
                 comment.getContent()
         );
+    }
+
+    @Transactional(readOnly = true)
+    public Page<CommentResponseDto> getComments(Long postId, Pageable pageable) {
+        return commentQueryRepository.findCommentsByPostId(postId, pageable)
+                .map(c -> {
+                    String profileImageUrl = imageService.createImageViewUrl(
+                            new CreateImageViewUrlRequestDto(ImageType.PROFILE, c.getMember().getId().longValue())
+                    ).getFirst().url();
+                    return new CommentResponseDto(c, profileImageUrl);
+                });
     }
 
     @Transactional
