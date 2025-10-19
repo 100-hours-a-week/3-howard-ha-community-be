7a2c0b7d9ec78b00efbc2c6fa82e1a110b16be6d
Howard <62272939+juyoungIt@users.noreply.github.com>
2025-10-19 09:33:24 +0900

    fix[#94]: ImageUrlResponseDto 에서 HttpMethod 직렬화 문제 해결

--- src/main/java/com/ktb/howard/ktb_community_server/image/dto/ImageUrlResponseDto.java
+++ src/main/java/com/ktb/howard/ktb_community_server/image/dto/ImageUrlResponseDto.java
@@ -1,5 +1,6 @@
 package com.ktb.howard.ktb_community_server.image.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.springframework.http.HttpMethod;
 
 import java.time.Instant;
@@ -8,6 +9,7 @@ public record ImageUrlResponseDto(
         String url,
         Long imageId,
         Integer sequence,
+        @JsonSerialize(using = com.fasterxml.jackson.databind.ser.std.ToStringSerializer.class)
         HttpMethod httpMethod,
         Instant expiresAt
 ) { }
